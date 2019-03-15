de65c2182b55abe5c1b3e56ee09e0f8bb733229a
Marina <marinaandjelkovic456@gmail.com>
2019-03-15 20:18:18 +0100

    zadatak 1

---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-collapse is-nav id="nav_collapse">
+      <b-navbar-nav>
+        <b-nav-item><router-link to="/customers">Customers</router-link></b-nav-item>
+        <b-nav-item><router-link to="/products">Products</router-link></b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,32 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+import BootstrapVue from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+import VueRouter from "vue-router";
+
+import AppCustomers from "./components/AppCustomers.vue";
+import AppProducts from "./components/AppProducts.vue";
+
+Vue.use(BootstrapVue);
+
+Vue.use(VueRouter);
+
+Vue.config.productionTip = false;
+
+const routes = [
+  { path: "/customers", component: AppCustomers },
+  { path: "/products", component: AppProducts }
+];
+
+const router = new VueRouter({
+  routes: routes,
+  mode: "history"
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  render: h => h(App)
+}).$mount("#app");
