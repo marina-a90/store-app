d1cbf1bdb81488f7381e762e6c43c821c061e232
Marina <marinaandjelkovic456@gmail.com>
2019-03-15 21:11:08 +0100

    zadatak 3

--- src/components/AppCustomers.vue
+++ src/components/AppCustomers.vue
@@ -1,12 +1,22 @@
 <template>
     <div>
         <h1>Customers</h1>
+        
         <ul>
             <li v-for="(customer, index) in customers" :key="index">
                 <div>{{ customer.name }} {{ customer.lastName }}</div>
                 <div><button @click="deleteCustomer(index)">Delete {{ customer.name }}</button></div>
             </li>
         </ul>
+
+        <form @submit.prevent="addCustomer">
+            <input type="text" v-model="customer.name" placeholder="name"/>
+            <input type="text" v-model="customer.lastName" placeholder="last name"/>
+            <input type="email" v-model="customer.email" placeholder="email"/>
+
+            <button type="submit">Add customer</button>
+        </form>
+
     </div>
 </template>
 
@@ -14,6 +24,14 @@
 export default {
   data() {
     return {
+      customer: [
+        {
+          name: "",
+          lastName: "",
+          email: "",
+          listaProizvoda: []
+        }
+      ],
       customers: [
         {
           name: "Pera",
@@ -51,6 +69,9 @@ export default {
       let customer = this.customers[index];
       this.customers.splice(index, 1);
       alert(`${customer.name} ${customer.lastName} has been removed.`);
+    },
+    addCustomer() {
+      this.customers.push({ ...this.customer });
     }
   }
 };
